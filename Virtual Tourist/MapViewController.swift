04de1faff3183ad3dfc147c5a09f08ee83c03d72
igorkurilenko <ikurilenko@live.com>
2015-09-11 17:04:40 +0300

    Add MapView and implement a current location discovery

--- Virtual Tourist/MapViewController.swift
+++ Virtual Tourist/MapViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import MapKit
 import CoreData
+import CoreLocation
 
 class MapViewController: UIViewController, NSFetchedResultsControllerDelegate {
-
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     lazy var sharedDataContext: NSManagedObjectContext = {
        return CoreDataStackManager.instance().managedObjectContext
     }()
@@ -18,6 +22,14 @@ class MapViewController: UIViewController, NSFetchedResultsControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        if(CLLocationManager.locationServicesEnabled()) {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            locationManager.startUpdatingLocation()
+        }
     }
 
     
@@ -28,4 +40,45 @@ class MapViewController: UIViewController, NSFetchedResultsControllerDelegate {
     }
 
 }
+
+extension MapViewController: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.pinColor = .Purple
+            
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        mapView.setCenterCoordinate(locationManager.location.coordinate, animated: false)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        print(error)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
 
